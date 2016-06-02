576a87122fe3d77102fa8da426e54a676233fdf8
Sajib Acharya <sajib@Macbooks-MacBook-Pro.local>
2016-06-02 17:33:26 +0530

    (v0.0.2) | MASTER

--- php/switchOneOn.php
+++ php/switchOneOn.php
@@ -1,4 +1,12 @@
+<!--
 <?php
-    $response = array('success' = true, 'message' = 'Switch One was successfully switched on!');
+
+    ini_set('display_startup_errors',1);
+    ini_set('display_errors',1);
+    error_reporting(-1);
+
+    $response = array('success' => true, 'message' => 'Switch One was successfully switched on!');
     echo json_encode($response);
 ?>
+-->
+<p>Awesome!</p>
