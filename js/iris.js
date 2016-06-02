64200a1689dca32fea7377dc1bf194feefa047da
Sajib Acharya <sajib@Macbooks-MacBook-Pro.local>
2016-06-02 17:08:47 +0530

    (v0.0.1) | MASTER

---
+++ js/iris.js
@@ -0,0 +1,231 @@
+///************
+// *@author: sajib
+// ************/
+
+$(function() {
+    var toggleSwitchOneOn = "http://localhost/iris/php/switchOneOn.php";
+    var toggleSwitchTwoOn = "";
+    var toggleSwitchThreeOn = "";
+    var toggleSwitchFourOn = "";
+    var toggleSwitchOneOff = "";
+    var toggleSwitchTwoOff = "";
+    var toggleSwitchThreeOff = "";
+    var toggleSwitchFourOff = "";
+    
+    $('#switch_one').bootstrapToggle();
+    $('#switch_two').bootstrapToggle();
+    $('#switch_three').bootstrapToggle();
+    $('#switch_four').bootstrapToggle();
+    
+    $("#switch_one").change(function() {
+        if($(this).prop('checked')) {
+            $.ajax({
+               type: 'GET',
+                url: toggleSwitchOneOn,
+                data: null,
+                success: function(response) {
+                    if(response.success) {
+                        swal("Success", response.message, "success");
+                    }
+                    else {
+                        //response was a failure
+                        swal("Error", "There was a problem with switch one.", "error");
+                        $("#switch_one").removeAttr('checked');
+                    }
+                },
+                error: function(status, errorThrown) {
+                    swal("Error", "The request could not be completed.", "error");
+                    $("#switch_one").removeAttr('checked');
+                }
+            });
+        }
+        else {
+            $.ajax({
+               type: 'GET',
+                url: toggleSwitchOneOff,
+                data: null,
+                success: function(response) {
+                    if(response.success) {
+                        if(response.success) {
+                            swal("Success", "Switch One was successfully switched off!", "success");
+                        }
+                        else {
+                            //response was a failure
+                            swal("Error", "There was a problem with switch one.", "error");
+                            $("#switch_one").prop('checked', true);
+                        }
+                    }
+                    else {
+                        //response was a failure
+                        swal("Error", "The request could not be completed.", "error");
+                        $("#switch_one").prop('checked', true);
+                    }
+                },
+                error: function(status, errorThrown) {
+                    swal("Error", "There was a problem connecting to the server.", "error");
+                    $("#switch_one").prop('checked', true);
+                }
+            });
+        }
+    })
+    
+    $("#switch_two").change(function() {
+        if($(this).prop('checked')) {
+            $.ajax({
+               type: 'GET',
+                url: toggleSwitchTwoOn,
+                data: null,
+                success: function(response) {
+                    if(response.success) {
+                        swal("Success", "Switch Two was successfully switched on!", "success");
+                    }
+                    else {
+                        //response was a failure
+                        swal("Error", "There was a problem with switch two.", "error");
+                        $("#switch_two").removeAttr('checked');
+                    }
+                },
+                error: function(status, errorThrown) {
+                    swal("Error", "The request could not be completed.", "error");
+                    $("#switch_two").removeAttr('checked');
+                }
+            });
+        }
+        else {
+            $.ajax({
+               type: 'GET',
+                url: toggleSwitchTwoOff,
+                data: null,
+                success: function(response) {
+                    if(response.success) {
+                        if(response.success) {
+                            swal("Success", "Switch Two was successfully switched off!", "success");
+                        }
+                        else {
+                            //response was a failure
+                            swal("Error", "There was a problem with switch two.", "error");
+                            $("#switch_two").prop('checked', true);
+                        }
+                    }
+                    else {
+                        //response was a failure
+                        swal("Error", "The request could not be completed.", "error");
+                        $("#switch_two").prop('checked', true);
+                    }
+                },
+                error: function(status, errorThrown) {
+                    swal("Error", "There was a problem connecting to the server.", "error");
+                    $("#switch_two").prop('checked', true);
+                }
+            });
+        }
+    })
+    
+    $("#switch_three").change(function() {
+        alert($(this).prop('checked'));
+        if($(this).prop('checked')) {
+            $.ajax({
+               type: 'GET',
+                url: toggleSwitchThreeOn,
+                data: null,
+                success: function(response) {
+                    if(response.success) {
+                        swal("Success", "Switch Three was successfully switched on!", "success");
+                    }
+                    else {
+                        swal("Error", "There was a problem with switch three.", "error");
+                        $("#switch_three").removeAttr('checked');
+                    }
+                },
+                error: function(status, errorThrown) {
+                    swal("Error", "The request could not be completed.", "error");
+                    $("#switch_three").removeAttr('checked');
+                }
+            });
+        }
+        else {
+            $.ajax({
+               type: 'GET',
+                url: toggleSwitchThreeOff,
+                data: null,
+                success: function(response) {
+                    if(response.success) {
+                        if(response.success) {
+                            swal("Success", "Switch Three was successfully switched off!", "success");
+                        }
+                        else {
+                            //response was a failure
+                            swal("Error", "There was a problem with switch three.", "error");
+                            $("#switch_three").prop('checked', true);
+                        }
+                    }
+                    else {
+                        //response was a failure
+                        swal("Error", "The request could not be completed.", "error");
+                        $("#switch_three").prop('checked', true);
+                    }
+                },
+                error: function(status, errorThrown) {
+                    swal("Error", "There was a problem connecting to the server.", "error");
+                    $("#switch_three").prop('checked', true);
+                }
+            });
+        }
+    })
+    
+    $("#switch_four").change(function() {
+        if($(this).prop('checked')) {
+            $.ajax({
+               type: 'GET',
+                url: toggleSwitchFourOn,
+                data: null,
+                success: function(response) {
+                    if(response.success) {
+                        swal("Success", "Switch Four was successfully switched on!", "success");
+                    }
+                    else {
+                        //response was a failure
+                        swal("Error", "There was a problem with switch four.", "error");
+                        $("#switch_four").removeAttr('checked');
+                    }
+                },
+                error: function(status, errorThrown) {
+                    swal("Error", "The request could not be completed.", "error");
+                    $("#switch_four").removeAttr('checked');
+                }
+            });
+        }
+        else {
+            $.ajax({
+               type: 'GET',
+                url: toggleSwitchFourOff,
+                data: null,
+                success: function(response) {
+                    if(response.success) {
+                        if(response.success) {
+                            swal("Success", "Switch Four was successfully switched off!", "success");
+                        }
+                        else {
+                            //response was a failure
+                            swal("Error", "There was a problem with switch one.", "error");
+                            $("#switch_four").prop('checked', true);
+                        }
+                    }
+                    else {
+                        //response was a failure
+                        swal("Error", "The request could not be completed.", "error");
+                        $("#switch_four").prop('checked', true);
+                    }
+                },
+                error: function(status, errorThrown) {
+                    swal("Error", "There was a problem connecting to the server.", "error");
+                    $("#switch_four").prop('checked', true);
+                }
+            });
+        }
+    })
+    
+    $("#live_feed").click(function() {
+        $("#sampleModal").modal('show');
+    })
+})
---
+++ php/switchOneOn.php
@@ -0,0 +1,4 @@
+<?php
+    $response = array('success' = true, 'message' = 'Switch One was successfully switched on!');
+    echo json_encode($response);
+?>
