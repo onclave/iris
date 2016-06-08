bba03ddbc454fac07761780abb09210b8e4ed298
Sajib Acharya <sajib@Macbooks-MacBook-Pro.local>
2016-06-09 03:10:13 +0530

    (v11.0.0)

--- android/Iris/app/src/main/java/com/onclave/iris/HomeFragment.java
+++ android/Iris/app/src/main/java/com/onclave/iris/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.onclave.iris;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
@@ -12,7 +13,23 @@ import android.widget.Switch;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class HomeFragment extends Fragment {
+
+    private ProgressDialog progressDialog;
+
+    private boolean s1state = false;
+    private boolean s2state = false;
+    private boolean s3state = false;
+    private boolean s4state = false;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -20,15 +37,25 @@ public class HomeFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
-        Switch switch1 = (Switch) view.findViewById(R.id.switch1);
+        final Switch switch1 = (Switch) view.findViewById(R.id.switch1);
         Switch switch2 = (Switch) view.findViewById(R.id.switch2);
         Switch switch3 = (Switch) view.findViewById(R.id.switch3);
         Switch switch4 = (Switch) view.findViewById(R.id.switch4);
 
+        switch1.setChecked(false);
+        switch2.setChecked(false);
+        switch3.setChecked(false);
+        switch4.setChecked(false);
+
         switch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if(isChecked) {
-                    Toast.makeText(getActivity().getApplicationContext(), "Switch One On", Toast.LENGTH_SHORT).show();
+                    progressDialog = new ProgressDialog(getActivity());
+                    progressDialog.setMessage("Logging in. Please wait...");
+                    progressDialog.setIndeterminate(false);
+                    progressDialog.setCancelable(false);
+                    progressDialog.show();
+                    registerAPIcall("http://httpbin.org/get?site=code&network=tutsplus", switch1);
                 } else {
                     Toast.makeText(getActivity().getApplicationContext(), "Switch One Off", Toast.LENGTH_SHORT).show();
                 }
@@ -66,5 +93,36 @@ public class HomeFragment extends Fragment {
         });
 
         return view;
+    }
+
+    public void registerAPIcall(final String url, final Switch switchButton) {
+        JsonObjectRequest jsonRequest = new JsonObjectRequest(Request.Method.GET,
+                url,
+                null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+//                            boolean success = response.getBoolean("success");
+//                            String message = response.getString("message");
+                            response = response.getJSONObject("args");
+                            String site = response.getString("site");
+                            progressDialog.dismiss();
+                            Toast.makeText(getActivity().getApplicationContext(), site, Toast.LENGTH_LONG).show();
+                            switchButton.setChecked(false);
+                        } catch (JSONException e) {
+                            //--handle exceptions here--
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        error.printStackTrace();
+                    }
+                });
+
+        Volley.newRequestQueue(getActivity().getApplicationContext()).add(jsonRequest);
     }
 }
